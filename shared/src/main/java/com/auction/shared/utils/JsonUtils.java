bbcaa0382c1e546b83e68b5298924d7c3b4d9462
Marhsie37 <25020267@vnu.edu.vn>
2026-03-31 21:15:55 +0700

    JsonUtils

---
+++ shared/src/main/java/com/auction/shared/utils/JsonUtils.java
@@ -0,0 +1,43 @@
+package com.auction.shared.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class JsonUtils {
+    private static final Gson gson = new GsonBuilder()
+            .setPrettyPrinting()
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+            .create();
+
+    public static String toJson(Object obj) {
+        return gson.toJson(obj);
+    }
+
+    public static <T> T fromJson(String json, Class<T> classOfT) {
+        return gson.fromJson(json, classOfT);
+    }
+
+    private static class LocalDateTimeAdapter extends com.google.gson.TypeAdapter<LocalDateTime> {
+        private final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+        @Override
+        public void write(com.google.gson.JsonWriter out, LocalDateTime value) throws java.io.IOException {
+            if (value == null) {
+                out.nullValue();
+            } else {
+                out.value(value.format(formatter));
+            }
+        }
+
+        @Override
+        public LocalDateTime read(com.google.gson.JsonReader in) throws java.io.IOException {
+            if (in.peek() == com.google.gson.JsonToken.NULL) {
+                in.nextNull();
+                return null;
+            }
+            return LocalDateTime.parse(in.nextString(), formatter);
+        }
+    }
+}
