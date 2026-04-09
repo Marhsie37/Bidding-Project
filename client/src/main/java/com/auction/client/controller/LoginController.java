1291bfd3da7046bfbcbda54e8df95d86d4673ff1
NguyenTai3101-tech <crissdevil12345@gamil.com>
2026-04-09 17:34:18 +0700

    BTL

--- client/src/main/java/com/auction/client/controller/LoginController.java
+++ client/src/main/java/com/auction/client/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.auction.client.controller;
+package Part1;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,40 +14,43 @@ import java.io.IOException;
 
 public class LoginController {
 
-    // 1. Khai báo 2 biến này để lấy chữ người dùng gõ vào (tên phải khớp fx:id)
+
     @FXML
     private TextField emailField;
     @FXML
     private TextField passwordField;
 
-    public static String emailHeThong = "admin";
-    public static String passHeThong = "123";
 
-    // Nút chuyển sang màn hình Đăng ký
+    public static String userHeThong = "admin_user";
+    public static String fullnameHeThong = "Quản trị viên";
+
+
+    public static String emailHeThong = "";
+    public static String passHeThong = "";
     @FXML
     public void goToRegisterScreen(ActionEvent event) {
         try {
-            Parent registerRoot = FXMLLoader.load(getClass().getResource("/Part1/Name.fxml"));
+            Parent registerRoot = FXMLLoader.load(getClass().getResource("/Part1/RegisterController.fxml"));
             Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
             window.setScene(new Scene(registerRoot));
-            window.setTitle("Nhập Họ và Tên");
+
+            window.setTitle("Đăng ký tài khoản");
             window.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    // Nút Confirm: Xử lý Đăng nhập
     @FXML
     public void handleLogin(ActionEvent event) {
         String emailDaNhap = emailField.getText().trim();
         String passDaNhap = passwordField.getText().trim();
 
-        // 2. SO SÁNH VỚI BỘ NHỚ TẠM
+
         if (emailDaNhap.equals(emailHeThong) && passDaNhap.equals(passHeThong)) {
 
             try {
-                Parent homeRoot = FXMLLoader.load(getClass().getResource("/Part2/Page.fxml"));
+                Parent homeRoot = FXMLLoader.load(getClass().getResource("/Part1/Page.fxml"));
                 Scene homeScene = new Scene(homeRoot);
                 Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
@@ -63,7 +66,8 @@ public class LoginController {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Lỗi đăng nhập");
             alert.setHeaderText(null);
-            alert.setContentText("Sai tài khoản hoặc mật khẩu!");
+
+            alert.setContentText("Sai tài khoản/mật khẩu! Nếu chưa có tài khoản, vui lòng bấm Register để tạo.");
             alert.showAndWait();
         }
     }
