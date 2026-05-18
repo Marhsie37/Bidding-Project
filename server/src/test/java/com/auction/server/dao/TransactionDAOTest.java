090ed459d6c12f6ad5f454eeb9286b796acf5b6f
UetDoran <leminhsonht2007@gmail.com>
2026-05-18 17:25:42 +0700

    update schema sql

--- server/src/test/java/com/auction/server/dao/TransactionDAOTest.java
+++ server/src/test/java/com/auction/server/dao/TransactionDAOTest.java
@@ -4,6 +4,7 @@ import com.auction.shared.model.User;
 import org.junit.jupiter.api.*;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.*;
